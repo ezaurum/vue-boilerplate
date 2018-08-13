4de1aa8e388121396c7ff5acd29ccdb431d9709b
조석규 <ezaurum@gmail.com>
2018-08-13 22:48:48 +0900

    Fix production config

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'production') {
             // mutate config for production...
-            //config.resolve.alias["store"] = path.resolve(__dirname, 'src/store.js')
+            config.resolve.alias["store"] = path.resolve(__dirname, 'src/store.js')
         } else {
             // mutate for development...
             config.resolve.alias["./store"] = path.resolve(__dirname, 'src/dummy-store.js')
