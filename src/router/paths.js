e0476652498bc3a982630e9b4fb50c1feecfe305
조석규 <ezaurum@gmail.com>
2018-08-14 14:43:01 +0900

    Add multi level views

--- src/router/paths.js
+++ src/router/paths.js
@@ -15,5 +15,28 @@ export default [
             /* webpackChunkName: "about" */
             /* webpackMode: "lazy" */
             `@/views/About.vue`)
+    },
+    {
+        path: '/pets',
+        component: () => import(
+            /* webpackChunkName: "pets" */
+            /* webpackMode: "lazy" */
+            `@/views/pets/Base.vue`),
+        children: [
+            {
+                path: '',
+                component: () => import(
+                    /* webpackChunkName: "pets" */
+                    /* webpackMode: "lazy" */
+                    `@/views/pets/List.vue`),
+            },
+            {
+                path: ':id',
+                component: () => import(
+                    /* webpackChunkName: "pets" */
+                    /* webpackMode: "lazy" */
+                    `@/views/pets/Pet.vue`),
+            }
+        ]
     }
 ]
---
+++ src/views/pets/List.vue
@@ -0,0 +1,7 @@
+<template>
+<ul>
+  <li><router-link to="pets/test0">test0</router-link></li>
+  <li><router-link to="pets/test1">test1</router-link></li>
+  <li><router-link to="pets/test2">test2</router-link></li>
+  <li><router-link to="pets/test3">test3</router-link></li>
+</ul></template>
