94df2aece94c4427ec390f70a2b0637027d25baa
조석규 <ezaurum@gmail.com>
2020-04-18 18:16:36 +0900

    feat: add tailwindcss

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+    <div class="flex-shrink-0">
+      <img class="h-12 w-12" src="../assets/logo.png" alt="ChitChat Logo">
+    </div>
+    <div class="ml-6 pt-1">
+      <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
+      <p class="text-base text-gray-600 leading-normal">You have a new message!</p>
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
   </div>
 </template>
 
@@ -16,3 +22,6 @@ export default {
   },
 }
 </script>
+<style lang="scss" scoped>
+@import "~@/assets/styles/style.css";
+</stylel>
--- src/views/Tsx.tsx
+++ src/views/Tsx.tsx
@@ -1,5 +1,6 @@
 import { VNode, CreateElement } from "vue"
 import { Component, Vue } from "vue-property-decorator"
+import "@/assets/styles/style.css"
 
 @Component
 export default class HelloWorld extends Vue {
@@ -10,6 +11,21 @@ export default class HelloWorld extends Vue {
     }
     return (
       <h1 style={style}>
+        <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+          <div class="flex-shrink-0">
+            <img
+              class="h-12 w-12"
+              src={require("../assets/logo.png")}
+              alt="ChitChat Logo"
+            />
+          </div>
+          <div class="ml-6 pt-1">
+            <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
+            <p class="text-base text-gray-600 leading-normal">
+              You have a new message!
+            </p>
+          </div>
+        </div>
         <Child>{this.$t("account.type.Admin")}</Child>
         <Child>{this.$t("account.type.Admin")}</Child>
       </h1>
